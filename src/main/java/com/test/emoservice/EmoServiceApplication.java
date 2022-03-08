1f22f7b95895e6aca12ed2123ced46ec4ce24bc4
Sisterin <1329853507@qq.com>
2022-03-08 21:18:19 +0800

    修改启动类，测试git提交

--- src/main/java/com/test/emoservice/EmoServiceApplication.java
+++ src/main/java/com/test/emoservice/EmoServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmoServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("新增打印");
 		SpringApplication.run(EmoServiceApplication.class, args);
 	}
 
